1ec4955105ef24042e9d7cd40dedc835228bdc66
familycomicsstudios <106832853+familycomicsstudios@users.noreply.github.com>
2022-06-05 18:34:43 -0500

    Update scratchmath.js

--- Code/scratchmath.js
+++ Code/scratchmath.js
@@ -48,6 +48,66 @@ class ScratchMath {
             }
           }
         },
+        {
+          opcode: 'gret',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] >= [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '5'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '5'
+            }
+          }
+        },
+        {
+          opcode: 'ltet',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] <= [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '4'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '4'
+            }
+          }
+        },
+        {
+          opcode: 'powersboi',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[ONE] ^ [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '2'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '3'
+            }
+          }
+        },
+        {
+          opcode: 'rootthingy',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[ONE] root of [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '2'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '3'
+            }
+          }
+        },
       ]
     };
   }
@@ -72,6 +132,18 @@ class ScratchMath {
   }
   negate(args) {
     return 0 - args.ONE;
+  }
+  gret(args) {
+    return args.ONE >= args.TWO;
+  }
+  ltet(args) {
+    return args.ONE <= args.TWO;
+  }
+  powersboi(args) {
+    return args.ONE ** args.TWO;
+  }
+  rootthingy(args) {
+    return args.TWO ** (1/args.ONE);
   }
 }
 Scratch.extensions.register(new ScratchMath());
